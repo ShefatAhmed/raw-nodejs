2f850dd06ebc5a819f26250474020e57da07f9b4
unknown <shefata69@gmail.com>
2025-05-07 21:27:48 +0600

    file system practice

---
+++ Server & Core Modules/fs.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+// fs.writeFileSync('myFile.txt', 'Hello World!'); // create a file with the name myFile.txt and write Hello World! in it
+// fs.appendFileSync('myFile.txt', 'Hello World2!'); // append Hello World! in the file myFile.txt
+// const data = fs.readFileSync('myFile.txt'); // read the file myFile.txt synchronously
+// fs.readFile('myFile.txt', (err, data) => {  
+// console.log(data.toString()); // convert the buffer to string and print it 
+// }); // read the file myFile.txt asynchronously
+// console.log(data.toString()); // convert the buffer to string and print it 
